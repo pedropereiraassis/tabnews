160f54d726956c74adeb7607a2a6afb7e2ad2582
Pedro Assis <pedroo16_@hotmail.com>
2025-03-25 13:00:54 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -38,6 +38,26 @@ export class ServiceError extends Error {
   }
 }
 
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "A validation error has occurred.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Adjust the data sent and try again.";
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("Method not allowed for this endpoint.");
