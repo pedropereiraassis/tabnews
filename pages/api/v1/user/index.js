2f95509451b9f3c1790a3482512fc8cd5bfe9e39
Pedro Assis <pedroo16_@hotmail.com>
2025-08-08 12:50:07 -0300

    fix: update `Cache-Control` header for `session renewal` response

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -19,5 +19,10 @@ async function getHandler(request, response) {
   const renewedSessionObject = await session.renew(sessionObject.id);
   controller.setSessionCookie(renewedSessionObject.token, response);
 
+  response.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, max-age=0, must-revalidate",
+  );
+
   return response.status(200).json(userFound);
 }
