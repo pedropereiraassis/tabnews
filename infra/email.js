5d50af7fe4e3b0f22756719a3b35206ec616536d
Pedro Assis <pedroo16_@hotmail.com>
2026-09-09 14:42:32 -0300

    refactor: improve error loggin in `email.send()`

--- infra/email.js
+++ infra/email.js
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { ServiceError } from "./errors.js";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
@@ -10,8 +11,17 @@ const transporter = nodemailer.createTransport({
   secure: process.env.NODE_ENV === "production",
 });
 
-async function send(emailOpts) {
-  await transporter.sendMail(emailOpts);
+async function send(mailOptions) {
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    throw new ServiceError({
+      message: "Não foi possível enviar o email.",
+      action: "Verifique se o serviço de email está disponível.",
+      cause: error,
+      context: mailOptions,
+    });
+  }
 }
 
 const email = {
--- infra/errors.js
+++ infra/errors.js
@@ -19,13 +19,14 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Service unavailable at the moment.", {
       cause,
     });
     this.name = "ServiceError";
-    this.action = "Verify if the service is available.";
+    this.action = action || "Verify if the service is available.";
     this.statusCode = 503;
+    this.context = context;
   }
 
   toJSON() {
@@ -34,6 +35,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
+      context: this.context,
     };
   }
 }
